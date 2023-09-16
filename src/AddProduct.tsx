516ee784358a4c85132346a01546c07d665367a4
ADEL ALATTIELI <2010181@acuq.ae>
2023-09-16 14:03:08 +0400

    .

--- src/AddProduct.tsx
+++ src/AddProduct.tsx
@@ -1,67 +1,115 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './AddProduct.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./AddProduct.css";
 
 function AddProduct() {
-    const [productName, setProductName] = useState('');
-    const [description, setDescription] = useState('');
-    const [costPrice, setCostPrice] = useState(0);
-    const [barcodePrice, setBarcodePrice] = useState(0);
-    const [photoUrl, setPhotoUrl] = useState('');
-    const [sku, setSku] = useState('');
-    const [barcode, setBarcode] = useState('');
-    const [availableQuantity, setAvailableQuantity] = useState(0);
-    const [category, setCategory] = useState('');
-    const [brand, setBrand] = useState('');
-    const [productAdded, setProductAdded] = useState(false); // New state to track product addition
+  const [productName, setProductName] = useState("");
+  const [description, setDescription] = useState("");
+  const [costPrice, setCostPrice] = useState(0);
+  const [barcodePrice, setBarcodePrice] = useState(0);
+  const [photoUrl, setPhotoUrl] = useState("");
+  const [sku, setSku] = useState("");
+  const [barcode, setBarcode] = useState("");
+  const [availableQuantity, setAvailableQuantity] = useState(0);
+  const [category, setCategory] = useState("");
+  const [brand, setBrand] = useState("");
+  const [productAdded, setProductAdded] = useState(false); // New state to track product addition
 
-    const handleAddStock = () => {
-        const newStock = {
-            productName,
-            description,
-            costPrice,
-            barcodePrice,
-            photoUrl,
-            sku,
-            barcode,
-            availableQuantity,
-            category,
-            brand
-        };
-        console.log(newStock);
-
-        // TODO: Add logic to save the new stock (e.g., send to server, update local state, etc.)
-
-        // Redirect to another page after adding, e.g., stock order list
-        setProductAdded(true); // Mark the product as added
+  const handleAddStock = () => {
+    const newStock = {
+      productName,
+      description,
+      costPrice,
+      barcodePrice,
+      photoUrl,
+      sku,
+      barcode,
+      availableQuantity,
+      category,
+      brand,
     };
+    console.log(newStock);
 
-    return (
-        <div className="add-product">
-            <h2>Add New Product</h2>
-            {productAdded ? (
-                <div>
-                    <p>Product added successfully!</p>
-                    <Link to="/">Go back to Products</Link>
-                </div>
-            ) : (
-            <form onSubmit={e => { e.preventDefault(); handleAddStock(); }}>
-                <input value={productName} onChange={e => setProductName(e.target.value)} placeholder="Product Name" />
-                <textarea value={description} onChange={e => setDescription(e.target.value)} placeholder="Description"></textarea>
-                <input type="number" value={costPrice} onChange={e => setCostPrice(Number(e.target.value))} placeholder="Cost Price" />
-                <input type="number" value={barcodePrice} onChange={e => setBarcodePrice(Number(e.target.value))} placeholder="Barcode Price" />
-                <input value={photoUrl} onChange={e => setPhotoUrl(e.target.value)} placeholder="Photo URL" />
-                <input value={sku} onChange={e => setSku(e.target.value)} placeholder="SKU" />
-                <input value={barcode} onChange={e => setBarcode(e.target.value)} placeholder="Barcode" />
-                <input type="number" value={availableQuantity} onChange={e => setAvailableQuantity(Number(e.target.value))} placeholder="Available Quantity" />
-                <input value={category} onChange={e => setCategory(e.target.value)} placeholder="Category" />
-                <input value={brand} onChange={e => setBrand(e.target.value)} placeholder="Brand" />
-                <button type="submit">Add Product</button>
-            </form>
-            )}
-            <Link to="/">Back to Products</Link>
+    // TODO: Add logic to save the new stock (e.g., send to server, update local state, etc.)
+
+    // Redirect to another page after adding, e.g., stock order list
+    setProductAdded(true); // Mark the product as added
+  };
+
+  return (
+    <div className="add-product">
+      <h2>Add New Product</h2>
+      {productAdded ? (
+        <div>
+          <p>Product added successfully!</p>
+          <Link to="/">Go back to Products</Link>
         </div>
-    );
+      ) : (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleAddStock();
+          }}
+        >
+          <input
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}
+            placeholder="Brand"
+          />
+          <input
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            placeholder="Category"
+          />
+          <input
+            value={photoUrl}
+            onChange={(e) => setPhotoUrl(e.target.value)}
+            placeholder="Photo URL"
+          />
+          <input
+            value={productName}
+            onChange={(e) => setProductName(e.target.value)}
+            placeholder="Product Name"
+          />
+          <input
+            value={sku}
+            onChange={(e) => setSku(e.target.value)}
+            placeholder="SKU"
+          />
+          <input
+            value={barcode}
+            onChange={(e) => setBarcode(e.target.value)}
+            placeholder="Barcode"
+          />
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Description"
+          ></textarea>
+          <input
+            type="number"
+            value={costPrice}
+            onChange={(e) => setCostPrice(Number(e.target.value))}
+            placeholder="Cost Price"
+          />
+          <input
+            type="number"
+            value={barcodePrice}
+            onChange={(e) => setBarcodePrice(Number(e.target.value))}
+            placeholder="Barcode Price"
+          />
+          <input
+            type="number"
+            value={availableQuantity}
+            onChange={(e) => setAvailableQuantity(Number(e.target.value))}
+            placeholder="Available Quantity"
+          />
+          <button type="submit">Add Product</button>
+        </form>
+      )}
+      <Link to="/">Back to Products</Link>
+    </div>
+  );
 }
 
 export default AddProduct;
