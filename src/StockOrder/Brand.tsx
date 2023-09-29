b6776f65267655af17e86c634e95eb5c4397c04e
ADEL ALATTIELI <2010181@acuq.ae>
2023-09-29 17:54:15 +0400

    ..

--- src/StockOrder/Brand.tsx
+++ src/StockOrder/Brand.tsx
@@ -1,19 +1,49 @@
-import React, { useState } from 'react';
-import "./Brand.css"
+import React, { useState } from "react";
+import "./Brand.css";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Link } from "react-router-dom";
 
-const BrandManager: React.FC = () => {
-  const [newBrandName, setNewBrandName] = useState<string>('');
-  const [Brands, setBrands] = useState<string[]>([]);
+
+
+const AddBrand: React.FC = () => {
+  const [newBrandName, setNewBrandName] = useState<string>("");
+  const [brands, setBrands] = useState<string[]>([]);
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
+  const [editingName, setEditingName] = useState<string>("");
 
   const handleAddBrand = () => {
-    if (newBrandName.trim() !== '') {
-      setBrands([...Brands, newBrandName]);
-      setNewBrandName('');
+    if (newBrandName.trim() !== "") {
+      setBrands([...brands, newBrandName]);
+      setNewBrandName("");
     }
+  };
+
+  const handleDeleteBrand = (index: number) => {
+    const newBrands = [...brands];
+    newBrands.splice(index, 1);
+    setBrands(newBrands);
+  };
+
+  const handleEditBrand = (index: number) => {
+    setEditingIndex(index);
+    setEditingName(brands[index]);
+  };
+
+  const handleUpdateBrand = () => {
+    const newCategories = [...brands];
+    newCategories[editingIndex as number] = editingName;
+    setBrands(newCategories);
+    setEditingIndex(null);
+    setEditingName("");
   };
 
   return (
     <div>
+      <Link to="/Stock-order" className="back-icon">
+      <ArrowBackIcon />
+    </Link>
       <h1>Brand Manager</h1>
       <div>
         <input
@@ -25,12 +55,41 @@ const BrandManager: React.FC = () => {
         <button onClick={handleAddBrand}>Add Brand</button>
       </div>
       <ul>
-        {Brands.map((brand, index) => (
-          <li key={index}>{brand}</li>
+        {brands.map((brand, index) => (
+          <li key={index}>
+            {editingIndex === index ? (
+              <div>
+                <input
+                  type="text"
+                  value={editingName}
+                  onChange={(e) => setEditingName(e.target.value)}
+                />
+                <button onClick={handleUpdateBrand}>Update</button>
+              </div>
+            ) : (
+              <>
+                {brand}
+                <div className="icon-container">
+                  <span
+                    className="clickable-icon edit-icon"
+                    onClick={() => handleEditBrand(index)}
+                  >
+                    <EditIcon />
+                  </span>
+                  <span
+                    className="clickable-icon delete-icon"
+                    onClick={() => handleDeleteBrand(index)}
+                  >
+                    <DeleteIcon />
+                  </span>
+                </div>
+              </>
+            )}
+          </li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default BrandManager;
+export default AddBrand;
--- src/StockOrder/Category.tsx
+++ src/StockOrder/Category.tsx
@@ -1,19 +1,49 @@
-import React, { useState } from 'react';
-import "./Category.css"
+import React, { useState } from "react";
+import "./Category.css";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Link } from "react-router-dom";
+
+
 
 const CategoryManager: React.FC = () => {
-  const [newCategoryName, setNewCategoryName] = useState<string>('');
+  const [newCategoryName, setNewCategoryName] = useState<string>("");
   const [categories, setCategories] = useState<string[]>([]);
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
+  const [editingName, setEditingName] = useState<string>("");
 
   const handleAddCategory = () => {
-    if (newCategoryName.trim() !== '') {
+    if (newCategoryName.trim() !== "") {
       setCategories([...categories, newCategoryName]);
-      setNewCategoryName('');
+      setNewCategoryName("");
     }
+  };
+
+  const handleDeleteCategory = (index: number) => {
+    const newCategories = [...categories];
+    newCategories.splice(index, 1);
+    setCategories(newCategories);
+  };
+
+  const handleEditCategory = (index: number) => {
+    setEditingIndex(index);
+    setEditingName(categories[index]);
+  };
+
+  const handleUpdateCategory = () => {
+    const newCategories = [...categories];
+    newCategories[editingIndex as number] = editingName;
+    setCategories(newCategories);
+    setEditingIndex(null);
+    setEditingName("");
   };
 
   return (
     <div>
+      <Link to="/Stock-order" className="back-icon">
+      <ArrowBackIcon />
+    </Link>
       <h1>Category Manager</h1>
       <div>
         <input
@@ -26,7 +56,36 @@ const CategoryManager: React.FC = () => {
       </div>
       <ul>
         {categories.map((category, index) => (
-          <li key={index}>{category}</li>
+          <li key={index}>
+            {editingIndex === index ? (
+              <div>
+                <input
+                  type="text"
+                  value={editingName}
+                  onChange={(e) => setEditingName(e.target.value)}
+                />
+                <button onClick={handleUpdateCategory}>Update</button>
+              </div>
+            ) : (
+              <>
+                {category}
+                <div className="icon-container">
+                  <span
+                    className="clickable-icon edit-icon"
+                    onClick={() => handleEditCategory(index)}
+                  >
+                    <EditIcon />
+                  </span>
+                  <span
+                    className="clickable-icon delete-icon"
+                    onClick={() => handleDeleteCategory(index)}
+                  >
+                    <DeleteIcon />
+                  </span>
+                </div>
+              </>
+            )}
+          </li>
         ))}
       </ul>
     </div>
